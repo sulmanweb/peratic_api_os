75a138d02b9eb9d0b901e0f005a25a75bfc29b02
Sulman Baig <sulmanweb@gmail.com>
2024-03-06 17:58:04 +0500

    SPT-281 - Adding Sign In mutation (#16)

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -1,23 +1,23 @@
 module Mutations
-  # Mutation to sign up a new user
-  class SignUp < BaseMutation
-    description 'Sign up a new user'
+  # Mutation to sign in a user
+  class SignIn < BaseMutation
+    description 'Sign in a user'
 
-    argument :email, String, required: true, description: 'Email of the user'
-    argument :name, String, required: true, description: 'Name of the user'
     argument :password, String, required: true, description: 'Password of the user'
     argument :username, String, required: true, description: 'Username of the user'
 
     field :errors, [String], null: true, description: 'Errors messages'
     field :success, Boolean, null: false, description: 'Success message'
+    field :token, String, null: true, description: 'Token'
     field :user, Types::UserType, null: true, description: 'User Type'
 
     def resolve(args)
-      result = Users::SignUpService.call(args)
+      result = Users::SignInService.call(args)
       {
         errors: result.errors,
         success: result.success,
-        user: result.data
+        token: result.data ? "Bearer #{result.data[:token]}" : nil,
+        user: result.data ? result.data[:user] : nil
       }
     end
   end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :sign_in, mutation: Mutations::SignIn, description: 'Sign in a user'
     field :sign_up, mutation: Mutations::SignUp, description: 'Sign up a new user'
   end
 end
