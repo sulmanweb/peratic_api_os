ba717f1f41c906334facb00eb2c0df8279e1250a
Sulman Baig <sulmanweb@gmail.com>
2024-03-10 20:45:22 +0500

    PT-4 - Add clockwork gem and configure cron service (#22)

---
+++ lib/clock.rb
@@ -0,0 +1,16 @@
+# This file is used to schedule background jobs using the clockwork gem.
+module Clock
+  require 'clockwork'
+  require 'active_support/time'
+  include Clockwork
+
+  handler do |job|
+    puts "Running #{job}"
+  end
+
+  every(1.minute, 'hello.world') { puts 'Hello, world!' }
+
+  every(1.day, 'destroy.soft_deleted_users', at: '12:05') do
+    DestroySoftDeletedUsersJob.perform_later
+  end
+end
