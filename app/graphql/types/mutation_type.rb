d0247320e852189c3a3fb529384d5fa1a982cc74
Sulman Baig <sulmanweb@gmail.com>
2024-03-10 21:17:00 +0500

    PT-7 - Add SendConfirmationEmail mutation and service

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@
 module Types
   class MutationType < Types::BaseObject
     field :self_destroy, mutation: Mutations::SelfDestroy, description: 'Destroy the user'
+    field :send_confirmation_email, mutation: Mutations::SendConfirmationEmail, description: 'Send confirmation email'
     field :sign_in, mutation: Mutations::SignIn, description: 'Sign in a user'
     field :sign_up, mutation: Mutations::SignUp, description: 'Sign up a new user'
   end
